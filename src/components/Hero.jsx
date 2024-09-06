9810213c1d285b8fd88ebf5ade4a08cdc1742a4a
Manish <106476756+Manish-S0@users.noreply.github.com>
2024-09-06 06:01:43 +0000

    remove earth canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-13  w-full flex justify-center items-center'>
         <a href='#about'>
